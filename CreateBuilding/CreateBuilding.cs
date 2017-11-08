3e7ef14552ede3e32fff098a58986735419d21e0
Robin Card <rod.love@btopenworld.com>
2017-11-08 13:57:11 +0000

    Works

--- CreateBuilding/CreateBuilding.cs
+++ CreateBuilding/CreateBuilding.cs
@@ -18,125 +18,75 @@ namespace N_N_Relationship
     {
         IOrganizationService service;
 
+        private ITracingService _tracingService;
+
         public void Execute(IServiceProvider serviceProvider)
         {
+            _tracingService = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+
             IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
             IOrganizationServiceFactory serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
             service = serviceFactory.CreateOrganizationService(context.UserId);
 
-            // Get the qualified lead
-            EntityReference leadid = (EntityReference)context.InputParameters["LeadId"];
-            Entity lead = service.Retrieve(leadid.LogicalName, leadid.Id, new ColumnSet(true));
-            try
+            if (context.InputParameters.Contains("Target") && context.InputParameters["Target"] is Entity)
             {
+                Entity entity = (Entity)context.InputParameters["Target"];
 
-                // Get the newly created account, contact, opportunity
-                //Entity contact = null;
-                Entity opportunity = null;
-                //Entity account = null;
-                foreach (EntityReference created in (IEnumerable<object>)context.OutputParameters["CreatedEntities"])
+                if (entity.LogicalName != "lead")
                 {
-                    switch (created.LogicalName)
-                    {
-                        //case "contact":
-                        //    contact = service.Retrieve("contact", created.Id, new ColumnSet(true));
-                        //    break;
-                        //case "account":
-                        //    account = service.Retrieve("account", created.Id, new ColumnSet(true));
-                        //    break;
-                        case "opportunity":
-                            opportunity = service.Retrieve("opportunity", created.Id, new ColumnSet(true));
-                            break;
-                    }
+                    _tracingService.Trace("Not a Lead");
+                    return;
+                }
+                
+                // Get the lead
+                //EntityReference leadid = (EntityReference)context.InputParameters["LeadId"];
+                Entity lead = service.Retrieve(entity.LogicalName, entity.Id, new ColumnSet(new string[] { "new_shortlist" }));
+
+                if (lead.Contains("new_shortlist"))
+                {
+                    _tracingService.Trace("Target Lead = {0}, Shortlist = {1}", lead.Id.ToString(), lead.Attributes["new_shortlist"].ToString());
+                }
+                else
+                {
+                    _tracingService.Trace("No Shortlist");
+                    return;
                 }
 
-                //create the Query
-                QueryExpression query = new QueryExpression();
-                query.EntityName = "new_building";
-                query.ColumnSet = new ColumnSet(true);
-                Relationship relationship = new Relationship();
-                query.Criteria = new FilterExpression();
-                query.Criteria.AddCondition(new ConditionExpression("statecode", ConditionOperator.Equal, "Active"));
-                relationship.SchemaName = "new_lead_new_building";
-                RelationshipQueryCollection relatedEntity = new RelationshipQueryCollection();
-                relatedEntity.Add(relationship, query);
-
-
-                //create the request
-                RetrieveRequest request = new RetrieveRequest();
-                request.RelatedEntitiesQuery = relatedEntity;
-                request.ColumnSet = new ColumnSet("leadid");
-                request.Target = new EntityReference { Id = leadid.Id, LogicalName = "lead" };
-
-                //execute the request
-                RetrieveResponse response = (RetrieveResponse)service.Execute(request);
-
-                // here you can check collection count
-                if (((DataCollection<Relationship, EntityCollection>)(((RelatedEntityCollection)(response.Entity.RelatedEntities)))).Contains(new Relationship("new_lead_new_building")) && ((DataCollection<Relationship, EntityCollection>)(((RelatedEntityCollection)(response.Entity.RelatedEntities))))[new Relationship("new_lead_new_building")].Entities.Count > 0)
+                try
                 {
-                    int x = ((DataCollection<Relationship, EntityCollection>)(((RelatedEntityCollection)(response.Entity.RelatedEntities))))[new Relationship("new_lead_new_building")].Entities.Count;
+                    string s = lead.Attributes["new_shortlist"].ToString();
+
+                    Guid[] Entities = Array.ConvertAll(s.Split(','), Guid.Parse); ;
+
+                    int x = Entities.Length; // number of entities
                     for (int i = 0; i < x; i++)
                     {
-                        Guid id = ((DataCollection<Relationship, EntityCollection>)(((RelatedEntityCollection)(response.Entity.RelatedEntities))))[new Relationship("new_lead_new_building")].Entities[i].Id;
+                        Guid id = Entities[i];
 
-
-                        /*if (contact != null)
+                        if (lead != null)
                         {
-                            AssociateRequest contactRel = new AssociateRequest
+                            AssociateRequest leadRel = new AssociateRequest
                             {
-                                Target = new EntityReference("contact", contact.Id),
+                                Target = new EntityReference("lead", lead.Id),
                                 RelatedEntities = new EntityReferenceCollection
-                                 {
-                                 new EntityReference("new_building", id)
-                                 },
-                                Relationship = new Relationship("new_contact_bank")
+                                    {
+                                    new EntityReference("new_building", id)
+                                    },
+                                Relationship = new Relationship("new_lead_new_building")
                             };
 
                             // Execute the request.
-                            service.Execute(contactRel);
-                        }*/
-
-                        /*if (account != null)
-                        {
-                            AssociateRequest accountRel = new AssociateRequest
-                            {
-                                Target = new EntityReference("account", account.Id),
-                                RelatedEntities = new EntityReferenceCollection
-                                 {
-                                 new EntityReference("new_building", id)
-                                 },
-                                Relationship = new Relationship("new_account_new_building")
-                            };
-
-                            // Execute the request.
-                            service.Execute(accountRel);
-                        }*/
-
-                        if (opportunity != null)
-                        {
-                            AssociateRequest opportunityRel = new AssociateRequest
-                            {
-                                Target = new EntityReference("opportunity", opportunity.Id),
-                                RelatedEntities = new EntityReferenceCollection
-                                 {
-                                 new EntityReference("new_building", id)
-                                 },
-                                Relationship = new Relationship("new_opportunity_new_building")
-                            };
-
-                            // Execute the request.
-                            service.Execute(opportunityRel);
+                            service.Execute(leadRel);
                         }
 
                     }
                 }
-            }
 
-            catch (Exception ex)
-            {
-                throw new InvalidPluginExecutionException("An error occured in the Plugin." + ex, ex);
+                catch (Exception ex)
+                {
+                    throw new InvalidPluginExecutionException("An error occured in the Plugin." + ex, ex);
+                }
             }
-
         }
     }
 }
